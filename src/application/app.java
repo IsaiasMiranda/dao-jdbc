4d27a971b409587cac7d1bd92f806ec9dabf9ed8
isaiasmiranda <isaias.analise@gmail.com>
2021-06-10 01:32:12 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/app.java
+++ src/application/app.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,8 @@ public class app {
 		Department department = new Department(1, "Livros");
 		Seller seller = new Seller(34, "Isaias Miranda", "isaias.analise@gmail.com", new Date(), 2500.60, department);
 
+		SellerDao sellerDao = DaoFactory.createSellerDao();
+		
 		System.out.println(seller);
 	}
 

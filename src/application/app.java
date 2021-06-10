a82ee903ac978838997e43b453d9a9ab5c147366
isaiasmiranda <isaias.analise@gmail.com>
2021-06-10 00:34:28 -0300

    Department entity class

---
+++ src/application/app.java
@@ -0,0 +1,13 @@
+package application;
+
+import model.entities.Department;
+
+public class app {
+
+	public static void main(String[] args) {
+
+		Department p = new Department(1, "Livros");
+		System.out.println(p);
+	}
+
+}

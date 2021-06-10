5608602a8b602b58e062f8213c65e166580123b3
isaiasmiranda <isaias.analise@gmail.com>
2021-06-10 00:52:04 -0300

    Seller entity class

--- src/application/app.java
+++ src/application/app.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class app {
 
 	public static void main(String[] args) {
 
-		Department p = new Department(1, "Livros");
-		System.out.println(p);
+		Department department = new Department(1, "Livros");
+		Seller seller = new Seller(34, "Isaias Miranda", "isaias.analise@gmail.com", new Date(), 2500.60, department);
+
+		System.out.println(seller);
 	}
 
 }

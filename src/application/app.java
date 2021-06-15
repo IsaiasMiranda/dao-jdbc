1d44547c4982555bb5be2294f9e0d7b655c7a942
isaiasmiranda <isaias.analise@gmail.com>
2021-06-15 00:44:03 -0300

    findById implementation

--- src/application/app.java
+++ src/application/app.java
@@ -1,22 +1,26 @@
 package application;
 
-import java.util.Date;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
-import model.entities.Department;
 import model.entities.Seller;
 
 public class app {
 
 	public static void main(String[] args) {
 
-		Department department = new Department(1, "Livros");
-		Seller seller = new Seller(34, "Isaias Miranda", "isaias.analise@gmail.com", new Date(), 2500.60, department);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.print("Enter seller code: ");
+		int id = sc.nextInt();
 
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
+
+		Seller seller = sellerDao.findById(id);
+
 		System.out.println(seller);
+
 	}
 
 }
--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,33 +1,82 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
 import model.entities.Seller;
 
-public class SellerDaoJDBC implements SellerDao{
+public class SellerDaoJDBC implements SellerDao {
+
+	private static Connection conn;
+
+	public SellerDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
 
 	@Override
 	public void insert(Seller seller) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void update(Seller seller) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void deleteById(Integer id) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public Seller findById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement pst = null;
+		ResultSet rs = null;
+		try {
+
+			String sql = "SELECT seller.*,department.Name as DepName\n" 
+							+ "	FROM seller INNER JOIN department\n"
+							+ "		ON seller.DepartmentId = department.Id\n" 
+							+ "			WHERE seller.Id = ?";
+
+			pst = conn.prepareStatement(sql);
+
+			pst.setInt(1, id);
+
+			rs = pst.executeQuery();
+
+			if (rs.next()) {
+				Department dep = new Department();
+				dep.setId(rs.getInt("DepartmentId"));
+				dep.setName(rs.getString("DepName"));
+
+				Seller seller = new Seller();
+				seller.setId(rs.getInt("Id"));
+				seller.setName(rs.getNString("Name"));
+				seller.setEmail(rs.getString("Email"));
+				seller.setBirthDate(rs.getDate("BirthDate"));
+				seller.setBaseSalary(rs.getDouble("baseSalary"));
+				seller.setDepartment(dep);
+				return seller;
+			}
+
+		} catch (SQLException e) {
+			throw new DbException("Error: " + e.getMessage());
+		} finally {
+			DB.closeStatement(pst);
+			DB.closeResultSet(rs);
+		}
+		
 		return null;
 	}
 
